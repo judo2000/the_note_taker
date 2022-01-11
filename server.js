add5445c9378ebc152b8e589dc4982c7ac706bd2
Stephen Moore <judo2000@comcast.net>
2022-01-11 07:21:57 -0700

    Add html routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const notes = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = process.env.port || 3001;
 const app = express();
@@ -13,7 +15,13 @@ app.use(express.static('public'));
 
 // routes
 // GET routes
+// Root Route
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')))
+
+// Notes Route
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
 
 app.listen(PORT, () =>
     console.log(`Listening at http://localhost:${PORT}!`)
