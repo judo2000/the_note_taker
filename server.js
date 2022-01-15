1008b448d1cfb9fb291b40ee16e2abbfbbef8437
Stephen Moore <judo2000@comcast.net>
2022-01-15 10:37:06 -0700

    update server.js

--- server.js
+++ server.js
@@ -7,16 +7,15 @@ const util = require("util");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// server static pages
+app.use(express.static("public"));
+
 // Helper method for generating unique ids
 const uuid = require("./helpers/uuid");
-const { json } = require("express");
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// server static pages
-app.use(express.static("public"));
 
 // routes
 // GET routes
@@ -32,23 +31,11 @@ app.get("/notes", (req, res) =>
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
 const writeToFile = (destination, content) =>
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
 const readAndAppend = (content, file) => {
   fs.readFile(file, "utf8", (err, data) => {
     if (err) {
@@ -88,7 +75,7 @@ app.post("/api/notes", (req, res) => {
     readAndAppend(newNote, "./db/db.json");
     res.json(`Note added successfully`);
   } else {
-    res.error("Error in adding note");
+    res.error("Error adding note");
   }
 });
 
